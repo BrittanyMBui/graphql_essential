bbf1aeb8c6db10ff50ba67c8fd7f58ea473b981b
brittanymbui <brittymbui@gmail.com>
2021-05-24 14:06:34 -0700

    Adds types to schema

--- schema.js
+++ schema.js
@@ -5,14 +5,21 @@ const schema = buildSchema(`
         id: ID
         firstName: String
         lastName: String
-        gender: String
+        gender: Gender
         age: Int
         email: String
+        contacts: [Contact]
+    }
+
+    type Contact {
+        firstName: String
+        lastName: String
     }
 
     enum Gender {
         MALE
         FEMALE
+        TRANS
         NONBINARY
         OTHER
     }
@@ -25,9 +32,15 @@ const schema = buildSchema(`
         id: ID
         firstName: String
         lastName: String
-        gender: String
+        gender: Gender
         age: Int
         email: String
+        contacts: [ContactInput]
+    }
+
+    input ContactInput {
+        firstName: String
+        lastName: String
     }
 
     type Mutation {
